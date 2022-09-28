2162ec91c74fb1b36891c48667ff493531c247dc
Will Paceley <pace0033@algonquinlive.com>
2022-09-28 12:08:08 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+/* --- JS CODE CHALLENGE --- */
+// The challenge today requires manipulating strings and arrays
+// 1.) Make an array of ONLY NAMES from the ourTeam string using .split()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split
+// 2.) On your new array, use .map() to create a new array with all names .toUpperCase()
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+// 3.) Convert the uppercased array into a string using .join(), with each name separated by a space
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join
+
+const ourTeam = "noah, evan, giselle, karla, karina, rabea, habib, diego, serdar, luciano, lizzie";
+/* --- YOUR CODE HERE --- */
+
+/* --- TESTS --- */
+const starting = "will, akshay, jasreet, vladymir";
+// you ending variable would contain the following string:
+// "WILL AKSHAY JASREET VLADYMIR"
